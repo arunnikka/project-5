29ec70834876037814a5e729c78cf4b5a2665800
arunnikka <160417720+arunnikka@users.noreply.github.com>
2025-03-20 15:42:16 +0700

    update code

--- src/Rekening.java
+++ src/Rekening.java
@@ -1,51 +1,59 @@
 public class Rekening {
     String namaPemilik;
-    String noRekening;
+    String nomorRekening;
     double saldo;
-
-    public Rekening(String namaPemilik, String noRekening, double saldo) {
+    
+    public Rekening(String namaPemilik, String nomorRekening, double saldo) {
         this.namaPemilik = namaPemilik;
-        this.noRekening = noRekening;
+        this.nomorRekening = nomorRekening;
         this.saldo = saldo;
     }
-
+    
     public String getNamaPemilik() {
         return namaPemilik;
     }
-
+    
     public void setNamaPemilik(String namaPemilik) {
         this.namaPemilik = namaPemilik;
     }
-
-    public String getNoRekening() {
-        return noRekening;
+    
+    public String getNomorRekening() {
+        return nomorRekening;
     }
-
-    public void setNoRekening(String noRekening) {
-        this.noRekening = noRekening;
+    
+    public void setNomorRekening(String nomorRekening) {
+        this.nomorRekening = nomorRekening;
     }
-
+    
     public double getSaldo() {
         return saldo;
     }
-
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
-    public double addSetor(double setor) {
-        this.saldo += setor;
-        return this.saldo;
+    
+    public void setor(double jumlah) {
+        if (jumlah > 0) {
+            saldo += jumlah;
+            System.out.println("Setor: " + jumlah);
+        } else {
+            System.out.println("Jumlah setor tidak valid.");
+        }
     }
-    public double addTarik(double tarik) {
-        this.saldo -= tarik;
-        return this.saldo;
+    
+    public void tarik(double jumlah) {
+        if (jumlah > saldo) {
+            System.out.println("Saldo tidak cukup untuk penarikan.");
+        } else {
+            saldo -= jumlah;
+            System.out.println("Tarik: " + jumlah);
+        }
     }
-
-    public void tampilkanInfo(){
-        System.out.println("Nama Pemilik: " + getNamaPemilik());
-        System.out.println("No Rekening: " + getNoRekening());
-        System.out.println("Saldo: " + getSaldo());
+    
+    public void tampilkanInfo() {
+        System.out.println("Nama Pemilik: " + namaPemilik);
+        System.out.println("Nomor Rekening: " + nomorRekening);
+        System.out.println("Saldo: " + saldo);
     }
-
 }
